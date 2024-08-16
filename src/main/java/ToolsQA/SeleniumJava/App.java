85dc3ec2865eee9ee535cee46ca9de8774748616
lanvu <lanvth97@gmail.com>
2024-08-16 16:37:33 +0700

    cross broswer

--- src/main/java/ToolsQA/SeleniumJava/App.java
+++ src/main/java/ToolsQA/SeleniumJava/App.java
@@ -2,21 +2,33 @@ package ToolsQA.SeleniumJava;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        Logger logger =  LogManager.getLogger(App.class);
-        
-        logger.info("this is a infor");
-        logger.error("this is a error");
-        
-        System.out.println( "finish" );
+    public static void main( String[] args ) throws Exception {
+        WebDriver drive;
+        String brower = "chrome";
+
+        if(brower.equalsIgnoreCase("firefox")){
+            drive = new FirefoxDriver();
+        }else if(brower.equalsIgnoreCase("chrome")){
+            drive  = new ChromeDriver();
+
+        }else if(brower.equalsIgnoreCase("Edge")){
+            drive = new EdgeDriver();
+        }else{
+            throw new Exception("Incorrect Browser");
+        }
+
+        drive.manage().window().maximize(); 
+
     }
 }
